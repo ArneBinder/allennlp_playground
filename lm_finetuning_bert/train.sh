67494742ee388cbec11498429c3d05cc9c01bb05
Arne Binder <arne.b.binder@gmail.com>
2019-06-12 15:50:58 +0200

    fixed CUDA_VISIBLE_DEVICES

--- lm_finetuning_bert/train.sh
+++ lm_finetuning_bert/train.sh
@@ -7,16 +7,18 @@ MODEL_OUT_DIR=models/coqa/bert-base-uncased/
 
 # use first argument as GPU devices, if available
 if [ -n "$1" ]; then
-    PY=CUDA_VISIBLE_DEVICES="$1" python
+    export CUDA_VISIBLE_DEVICES=$1
     echo "use CUDA_VISIBLE_DEVICES=$1"
 else
-    PY=python
     echo "WARNING: CUDA_VISIBLE_DEVICES is not set!"
 fi
 
+CMD="python lm_finetuning_bert/finetune_on_pregenerated.py"
+echo "execute: $CMD"
+
 #cd allennlp_playground
 mkdir -p $MODEL_OUT_DIR
-$PY lm_finetuning_bert/finetune_on_pregenerated.py \
+$CMD \
 --pregenerated_data $TRAIN_DIR \
 --bert_model bert-base-uncased \
 --do_lower_case \
