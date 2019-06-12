8dc3446c608fab0c8d8e711eb7ec84d3c10dde77
Arne Binder <arne.b.binder@gmail.com>
2019-06-12 15:01:18 +0200

    fixed script

--- lm_finetuning_bert/prepare_coqa_data.sh
+++ lm_finetuning_bert/prepare_coqa_data.sh
@@ -2,14 +2,16 @@
 
 # Prepare CoQA data for language model train setting.
 
-COQA_TRAIN_DATA=/home/abinder/datasets/CoQA/coqa-train-v1.0.json
-TRAIN_PATH=training/coqa/
 # do not forget to add/remove parameter "--do_lower_case" to python command, if necessary!
 BERT_MODEL=bert-base-uncased
 
+COQA_TRAIN_DATA=/home/abinder/datasets/CoQA/coqa-train-v1.0.json
+TRAIN_PATH=training/coqa/$BERT_MODEL/
+
+
 #cd allennlp_playground
 # generate CoQA training data
-mkdir -p $TRAIN_PATH
+mkdir -p $TRAIN_PATH/$BERT_MODEL
 echo "prepare training data with: $COQA_TRAIN_DATA and write to: $TRAIN_PATH"
 python lm_finetuning_bert/pregenerate_training_data.py \
 --train_corpus $COQA_TRAIN_DATA \
