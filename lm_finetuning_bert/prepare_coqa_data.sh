25c42013867489948f4643fb76a3c7535fd1cc53
Arne Binder <arne.b.binder@gmail.com>
2019-06-12 13:30:37 +0200

    update scripts

--- lm_finetuning_bert/prepare_coqa_data.sh
+++ lm_finetuning_bert/prepare_coqa_data.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Prepare CoQA data for language model train setting.
+
 COQA_TRAIN_DATA=/home/abinder/datasets/CoQA/coqa-train-v1.0.json
 TRAIN_PATH=training/coqa/
+# do not forget to add/remove parameter "--do_lower_case" to python command, if necessary!
+BERT_MODEL=bert-base-uncased
 
 #cd allennlp_playground
 # generate CoQA training data
@@ -10,7 +14,7 @@ echo "prepare training data with: $COQA_TRAIN_DATA and write to: $TRAIN_PATH"
 python lm_finetuning_bert/pregenerate_training_data.py \
 --train_corpus $COQA_TRAIN_DATA \
 --document_loader coqa_document_loader \
---bert_model bert-base-uncased \
+--bert_model $BERT_MODEL \
 --do_lower_case \
 --output_dir $TRAIN_PATH \
 --epochs_to_generate 3 \
--- lm_finetuning_bert/train.sh
+++ lm_finetuning_bert/train.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-TRAIN_DIR=training/coqa/
-MODEL_OUT_DIR=models/coqa_lm/
+# Train (fine-tune) BERT on GPUs (e.g. with prepared CoQA data).
+
+TRAIN_DIR=training/coqa/bert-base-uncased/
+MODEL_OUT_DIR=models/coqa/bert-base-uncased/
+
+GPUS=0,1
 
 #cd allennlp_playground
-python lm_finetuning_bert/finetune_on_pregenerated.py \
+mkdir -p $MODEL_OUT_DIR
+CUDA_VISIBLE_DEVICES=$GPUS python lm_finetuning_bert/finetune_on_pregenerated.py \
 --pregenerated_data $TRAIN_DIR \
 --bert_model bert-base-uncased \
 --do_lower_case \
